7e586ad36e59524186dddbef002327dd3eb3c36c
renyen00 <renyen00@gmail.com>
2022-05-05 01:56:17 +0800

    bars  各種按鈕縮合 店鋪頁效果

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-// 04-26 上午1:15處  按鈕縮合
+//  rwd h3按鈕縮合
+
+let footer = document.getElementsByTagName('footer')[0].querySelectorAll('nav');
+// console.log(footer);
+for(let i = 0; i < footer.length ; i++){
+    let fclick = footer[i].querySelector('p');
+
+    fclick.addEventListener('click',function(){
+
+        this.nextElementSibling.classList.toggle('-on');
+        
+    })
+}
+
+
+
+// 漢堡
+let bar = document.getElementsByClassName('bars')[0];
+bar.addEventListener('click',function(){
+    let topmain = document.querySelector('ul');
+    topmain.classList.toggle('-on')
+})
