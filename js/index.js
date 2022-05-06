9a2b9ad11c7881eb7c899032e2d7670edbd1f489
renyen00 <renyen00@gmail.com>
2022-05-06 19:24:35 +0800

    購！物！車！

--- js/index.js
+++ js/index.js
@@ -20,3 +20,27 @@ bar.addEventListener('click',function(){
     let topmain = document.querySelector('ul');
     topmain.classList.toggle('-on')
 })
+
+
+// 購物車相關
+
+let cart_count = document.getElementsByClassName('cart_count')[0];
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    change();
+});
+
+function change(){
+    let lists =JSON.parse(sessionStorage.getItem('lists'));
+    cart_count.innerText = lists.length;  
+    if (lists){
+        // console.log(cart_count.innerText);
+        if(cart_count.innerText == 0){
+            cart_count.style.display = 'none';
+        }else{
+            cart_count.style.display = 'block';
+        }
+         
+    }
+}
